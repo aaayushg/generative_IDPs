edab4f42cecca08ce3aa4904ff5c8806ca08e1fd
Aayush Gupta <47544727+aaayushg@users.noreply.github.com>
2022-01-06 21:30:44 -0600

    Update generate_new_IDPs.py

--- Implementation_codes/generate_new_IDPs.py
+++ Implementation_codes/generate_new_IDPs.py
@@ -5,7 +5,7 @@ import numpy as np
 from copy import deepcopy
 from keras.models import Model, Sequential, load_model
 
-fname = '/home/aayush/Documents/input_data/chiz/run1/run1_trunc.pdb'
+fname = 'chiz/run1/run1_trunc.pdb'
 
 M = bb.Molecule()
 M.import_pdb(fname)
@@ -51,10 +51,8 @@ encoded_train_prots = encoder.predict(x_train)
 
 mean_a= np.mean(encoded_train_prots, axis=0)
 cov=np.cov(np.array(encoded_train_prots).T)
-print(mean_a)
-print(cov)
 
-#Sample criteria 1 - only positive value 
+#Sample with criteria - only positive value 
 def cov_prots_gen1(n):
     cov_prots=[]
     while len(cov_prots) < n:
@@ -66,19 +64,9 @@ def cov_prots_gen1(n):
                     continue
     return np.concatenate(cov_prots, axis=0)
 
-##Sample criteria 1 - only positive value 
-def cov_prots_gen2(n):
-    cov_prots=[]
-    while len(cov_prots) < n:
-            x=np.random.multivariate_normal(mean_a,cov,size=1)
-            print(x)
-            np.add(x,abs(np.min(x)))
-            cov_prots.append(np.add(x,abs(np.min(x))))
-    return np.vstack(cov_prots)
-
 #cov_prots=cov_prots_gen2(43500)
 
-#or sample randomly
+#or just sample randomly
 cov_prots = np.random.multivariate_normal(mean_a,cov,size=101500)
 
 np.savetxt("cov_positive_set_101500.dat", cov_prots)
@@ -88,10 +76,3 @@ decoded_reshaped2 = decoded_cov_prots.reshape(decoded_cov_prots.shape[0], int(de
 M5 = M.get_subset(idxs=idx, conformations=[0])
 M5.coordinates = decoded_reshaped2*scaling2
 M5.write_pdb("cov_positive_101500.pdb")
-
-#save all encodings
-#np.savetxt("encoded_training_set.dat", encoded_train_prots)
-#np.savetxt("%s/%s_encoded_test_set.dat"%(outfolder, outlabel), encoded_test_prots)
-#np.savetxt("%s/%s_new_set.dat"%(outfolder, outlabel), new_prots)
-#np.savetxt("cov_positive_set.dat", cov_prots)
-
